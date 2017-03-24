98cb30520208d83c98b97f6c8054cb6cb76272cb
ylt94 <857425891@qq.com>
2017-03-24 23:41:19 +0800

    11

--- routes/api.php
+++ routes/api.php
@@ -25,14 +25,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['prefix' => 'user','namespace'=>'User'], function () {
-    Route::get('/member', 'MemberController@list');
+    Route::get('/member', 'MemberController@lists');
     Route::post('/member/add', 'MemberController@add');
     Route::post('/member/update', 'MemberController@update');
     Route::get('/member/del', 'MemberController@del');
     Route::get('/member/search', 'MemberController@search');
 
 
-    Route::get('/repairer', 'RepairerController@list');
+    Route::get('/repairer', 'RepairerController@lists');
     Route::post('/repairer/add', 'RepairerController@add');
     Route::post('/repairer/update', 'RepairerController@update');
     Route::get('/repairer/del', 'RepairerController@del');
@@ -41,14 +41,14 @@ Route::group(['prefix' => 'user','namespace'=>'User'], function () {
 });
 
 Route::group(['prefix' => 'goods','namespace'=>'Goods'], function () {
-    Route::get('', 'GoodsController@list');
+    Route::get('', 'GoodsController@lists');
     Route::post('/add', 'GoodsController@add');
     Route::post('/update', 'GoodsController@update');
     Route::get('/del', 'GoodsController@del');
     Route::get('/search', 'GoodsController@search');
 });
 Route::group(['prefix' => 'order'], function () {
-    Route::get('/list', 'Order\OrderController@list');
+    Route::get('/list', 'Order\OrderController@lists');
     Route::get('/list/readorder', 'Order\OrderController@readorder');
     Route::get('/list/search', 'Order\OrderController@search');
     Route::get('/list/selectedrepairer', 'Order\OrderController@selectedrepairer');
@@ -62,7 +62,7 @@ Route::group(['prefix' => 'order'], function () {
 
 
 Route::group(['prefix' => 'fitting'], function () {
-    Route::get('/list', 'Fitting\FittingController@list');
+    Route::get('/list', 'Fitting\FittingController@lists');
     Route::post('/list/add', 'Fitting\FittingController@add');
     Route::post('/list/update', 'Fitting\FittingController@update');
     Route::get('/list/del', 'Fitting\OFittingController@del');
@@ -80,7 +80,7 @@ Route::group(['prefix' => 'fitting'], function () {
 
 
 Route::group(['prefix' => 'information','namespace'=>'Information'], function () {
-    Route::get('/station', 'StationController@list');
+    Route::get('/station', 'StationController@lists');
     Route::post('/station/add', 'StationController@add');
     Route::post('/station/update', 'StationController@update');
     Route::get('/station/del', 'StationController@del');
